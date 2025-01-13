5a906589291e6a87d00158f5a00f002796d29a6e
wassimmizouriiiiiii <wassimmizouri1@gmail.com>
2025-01-13 11:49:53 +0100

    Ajout du comp formulaire

---
+++ src/app/formulaire/formulaire.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulaire',
+  imports: [],
+  templateUrl: './formulaire.component.html',
+  styleUrl: './formulaire.component.css'
+})
+export class FormulaireComponent {
+
+}
